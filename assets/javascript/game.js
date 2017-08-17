280925533de1d2aad4184b81b9d92095b2b256af
Elliott Wendel <ewendel88@gmail.com>
2017-08-17 15:18:23 -0500

    Working, no style

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,51 +1,56 @@
-var wordList = ["Top Gun",
-	// "Reaganomics",
-	// "As If",
-	// "Awesome",
-	// "Bad",
-	// "Barney",
-	// "Bodacious",
-	// "Bogus",
-	// "Bonus",
-	// "Boss",
-	"Bow-Head",
-	// "Brody",
-	// "Take a Chill Pill",
-	// "Chillin",
-	// "Cool Beans",
-	// "Deck",
-	// "Def",
-	"Don't Have a Cow",
-	// "Dudical",
-	// "Dweeb",
-	// "Fer Sure",
-	// "Fresh",
-	// "Gag Me With a Spoon",
-	// "Generic",
-	// "Gnarly",
-	// "Grody",
-	// "Gross Me Out the Door",
-	// "Make Me Barf",
-	// "McFly",
-	// "Nice Play, Shakespeare",
-	// "Preppie",
-	// "Radical",
-	// "Stoked",
-	// "Val",
-	// "Wicked",
-	// "Yuppie",
-]; // list of word to be chossen from to guess
-var guessWordIndex = Math.floor(Math.random() * wordList.length); // random number to pick the word from wordlist
+var wordList = [ // list of word to be chossen from to guess
+"Top Gun",
+"Reaganomics",
+"As If",
+"Awesome",
+"Bad",
+"Barney",
+"Bodacious",
+"Bogus",
+"Bonus",
+"Boss",
+"Bow-Head",
+"Brody",
+"Take a Chill Pill",
+"Chillin",
+"Cool Beans",
+"Deck",
+"Def",
+"Don't Have a Cow",
+"Dudical",
+"Dweeb",
+"Fer Sure",
+"Fresh",
+"Gag Me With a Spoon",
+"Generic",
+"Gnarly",
+"Grody",
+"Gross Me Out the Door",
+"Make Me Barf",
+"McFly",
+"Nice Play, Shakespeare",
+"Preppie",
+"Radical",
+"Stoked",
+"Val",
+"Wicked",
+"Yuppie",
+];
+
+var guessWordIndex = Math.floor(Math.random() * wordList.length); // random number to pick the word from wordList
 var guessWord = wordList[guessWordIndex]; // Randomly chosen word for player to guess
 var guessWordLowerCase = guessWord.toLowerCase(); // lower case version of guessWord
 var lettersGuessed = []; // gameplay built list of letters the player has guessed
 var playing = false; // playing boolean
 var answer = []; // guessWord in array form used to build the word as the player guesses correct letters
-var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",]
+var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",]; // alphabet to campare against
+var lives = 10; // lives 
+var correctGuess = false; // correct guess or not
+var missingLetters; // number of missing letters
 
-// console.log(); // debug
+console.log("Answer: " + guessWord); // debug
 
-for (var i = 0; i <guessWord.length; i++) { // fill in answer[] with _ or " "
+for (var i = 0; i <guessWord.length; i++) { // fill in answer with spaces or punctiation
 	if (guessWord[i] === " ") {
 		answer.splice(i, 1, "&nbsp");
 	}
@@ -55,46 +60,103 @@ for (var i = 0; i <guessWord.length; i++) { // fill in answer[] with _ or " "
 	else if (guessWord[i] === "-") {
 		answer.splice(i, 1, "-");	
 	}
+	else if (guessWord[i] === ",") {
+		answer.splice(i, 1, ",");	
+	}
 	else {
-		answer.splice(i, 1, "|");
+		answer.splice(i, 1, "?");
 	}
 }
 
 document.onkeyup = function(event){ // on key pressed
 
-	if (playing === true) { // if game is being played (after title screen)
-		console.log("Pushing: " + event.key);
-		pushLetter(); // add guessed letters to running list of the letters already guessed
+	var guess = alphabet.find(isLetter); // variable containing the letter just pressed
+	console.log("Key: " + event.key);
 
-		for  (var answerLetter = 0; answerLetter < guessWord.length; answerLetter++) { // check each letter of guessWord against the letter just guessed
-				if (event.key === guessWordLowerCase[answerLetter]) { // if the letters match
-					answer.splice(answerLetter, 1, guessWord[answerLetter]); // change the letter at the answerLetter index to the guessed letter
-				}
-			}
-		document.querySelector("#game").innerHTML = "<h2>Your word is : " + answer.join("") + "</h2>" + // new HTML game div
-					                                "<h3>Already Guessed: " + lettersGuessed.toString().toUpperCase() + "</h3>"; // new HTML game div	
-					                            }
-	else if (event.key === "Enter" && playing === false) { // before starting the game (tittle screen) and enter key is pressed
-		playing = true // set playing to true
-		document.querySelector("#game").innerHTML = "<h2>Your word is : " + answer.join("") + "</h2>"; // new HTML game div
+	if (event.key === "Enter" && playing === false) { // before starting the game (tittle screen) and enter key is pressed
+		console.log("Starting new Game.");
+		document.querySelector("#game").innerHTML = // new HTML game div
+		"<h2>Your word is : " + answer.join("") + "</h2>" +
+		"<h3>Press a key to guess a letter.</h3>";
+		playing = true; // set playing to true
 	}
 
-	function pushLetter() {
-		console.log("Run pushLetter.");
-		for (var isGuessed = 0; isGuessed < lettersGuessed.length; isGuessed++) {
-			console.log("Searching lettersGuessed.");
-			if (alphabet.find(findkey) === lettersGuessed[isGuessed]) {
-				console.log(alphabet.find(findkey) + " already in list.");
+	else if (playing === true) { // if game is being played (after title screen)
+
+		if (lettersGuessed.some(notInList)) { // if the letter is already in the list
+			return; // do nothing
+		}
+		else {			
+			lettersGuessed.push(guess); // add guessed letters to running list of the letters already guessed
+		}
+
+		for  (var i = 0; i < guessWord.length; i++) { // check each letter of guessWord against the letter just guessed
+			if (guess === guessWordLowerCase[i]) { // if the letters match
+				answer.splice(i, 1, guessWord[i]); // change the letter at the i index to the guessed letter from the word
+				correctGuess = true;
 			}
-			else {
-				console.log(alphabet.find(findkey) + " is a new letter.");
-				lettersGuessed.push(alphabet.find(findKey));
-			}
-		}				 
+		}
+
+		countMissingLetters(); // adjust missingLetters variable
+
+		if (correctGuess === false) { // if letter was not in word
+			lives--;
+		}
+
+		correctGuess = false;
+
+		document.querySelector("#game").innerHTML = // Display game
+		"<h2>Your word is : " + answer.join("") + "</h2>" + 
+		"<h3>Already Guessed: " + lettersGuessed.sort().toString().toUpperCase() + "</h3>" + 
+		"<h4>Lives remaining: " + lives + "</h4>";
+
 	}
 
-	function findKey(key) {
-		return key === event.key;
+	if (missingLetters <= 0) { // If no missingLetters left (Win)
+		playing = false;
+
+		document.querySelector("#game").innerHTML = // Congratulations screen
+		"<h1>RADICAL TO THE MAX!!!!</h1>" + 
+		"<h2>You won!</h2>" + 
+		"<h3>The word was " + guessWord + "</h3>" +
+		"<h3>Now go get yourself a Crystal Pepsi. You deserve it.</h3>" +
+		"<h5>Press Enter to play again.</h5>";
+
+		if (event.key === "Enter") { // hit enter to reload the page
+			location.reload(true);
+		}
+	}
+
+	if (lives <= 0) { // If no lives left (lose)
+		playing = false;
+
+		document.querySelector("#game").innerHTML = // Game over screen
+		"<h1>GAME OVER!!!!</h1>" +
+		"<h3>The word was " + guessWord + "</h3>" +
+		"<h2>Like, totally bogus ya jabroni!</h2>" +
+		"<h5>Press Enter to play again.</h5>";
+
+		if (event.key === "Enter") { // hit enter to reload the page
+			location.reload(true);
+		}
+	}
+
+
+	function isLetter(key) {
+		return key === event.key
+	}
+
+	function notInList(letter) {
+		return letter === guess
+	}
+	function countMissingLetters() {
+		missingLetters = 0;
+		for (var i = 0; i < answer.length; i++) {
+			if (answer[i] === "?") {
+				missingLetters++;
+			}
+		}
+		console.log("Missing letters: " + missingLetters);
 	}
 }
 
